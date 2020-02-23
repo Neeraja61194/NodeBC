9528d406b98b491f767e7981ccf60949be3b5294
Neeraja Neelakantan <neerajajithinp@gmail.com>
2020-02-23 15:16:49 -0800

    modifying ex 17

--- Assignments/BrowserAPI/ex_17/script.js
+++ Assignments/BrowserAPI/ex_17/script.js
@@ -5,3 +5,12 @@ if (className == 'blue' || className == 'green' || className == 'red'){
 } else{
     window.alert("Please input a valid colour only");
 }
+
+/* 
+Ask the user to select a color showing the following message:
+Please choose & input one of the following colors: green, blue or red
+Select the first link element
+Add the user selected color as class name to the selected element (first link)
+Avoid adding the class if the user inputs a color that's not an option
+So if the user selects blue as color we need to add the blue class to the first link
+*/
