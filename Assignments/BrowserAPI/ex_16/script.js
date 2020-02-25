979a6109eca5cb2955f1cbc97dc5d39e099fd729
Neeraja Neelakantan <neerajajithinp@gmail.com>
2020-02-24 18:41:48 -0800

    modified 16

--- Assignments/BrowserAPI/ex_16/script.js
+++ Assignments/BrowserAPI/ex_16/script.js
@@ -5,3 +5,9 @@ const classValue = element.getAttribute('class');
 
 console.log(href);
 console.log(classValue);
+
+/* 
+Select the first link element
+Show the href and class attributes values
+Use any of the ways that you know
+ */
